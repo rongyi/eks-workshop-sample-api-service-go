e99150a7dda12280fc5a5922d339a578ce1ef727
rongyi <rongyi@onchain.com>
2019-10-25 15:10:39 +0800

    Just to trigger another commit

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World from rongyi"}
+		res := &response{Message: "Hello World from rongyi, just trigger another build"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
